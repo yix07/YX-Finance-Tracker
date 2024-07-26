41edeb1d832c1ab6b874c087d880b87c936d3b69
Yixiang Chen <yee0730@gmail.com>
2024-07-26 16:02:33 -0400

    Accounts POST API

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function Home() {
+  const { onOpen } = useNewAccount();
+
   return (
     <div>
-      Home Page
+      <Button onClick={onOpen}>
+        Add an account
+      </Button>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import {
   ClerkProvider
 } from '@clerk/nextjs'
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +25,8 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <QueryProvider>
+            <SheetProvider/>
+            <Toaster/>
             {children}
           </QueryProvider>
         </body>
