578ac2b25e74b666298ed95a50942b6259645201
Yixiang Chen <yee0730@gmail.com>
2024-07-31 20:44:22 -0400

    Transactions API enpoints and hooks

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,5 +1,6 @@
 import accounts  from "./accounts";
 import categories from "./categories"
+import transactions from "./transactions";
 
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
@@ -11,6 +12,7 @@ const app = new Hono().basePath('/api')
 const routes = app
     .route("/accounts", accounts)
     .route("/categories", categories)
+    .route("/transactions", transactions);
 
 export const GET = handle(app)
 export const POST = handle(app)
