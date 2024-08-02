c91479f8af240ccca12d84593310ebb53c502d39
Yixiang Chen <yee0730@gmail.com>
2024-08-02 15:31:11 -0400

    CSV import feature

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -10,11 +10,18 @@ import {
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useBulkCreateTransactions } from "@/features/transactions/api/use-bulk-create-transactions";
+import { useSelectAccount } from "@/features/accounts/hooks/use-select-account";
 import { Loader2, Plus } from "lucide-react";
 
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+import { transactions as transactionSchema } from "@/db/schema";
+import { toast } from "sonner";
 
 enum VARIANTS {
     LIST = "LIST",
@@ -28,11 +35,47 @@ enum VARIANTS {
   };
 
 const TransactionsPage = () => {
+    const [AccountDialog, confirm] = useSelectAccount();
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    };
+
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    }
+
     const newTransaction = useNewTransaction();
+    const createTransactions = useBulkCreateTransactions();
     const transactionsQuery = useGetTransactions();
     const transactions = transactionsQuery.data || [];
     const deleteTransactions = useBulkDeleteTransactions();
     const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending;
+
+    const onSubmitImport = async (
+        values: typeof transactionSchema.$inferInsert[],
+    ) => {
+        const accountId = await confirm();
+
+        if (!accountId) {
+            return toast.error("Please select an account to continue.");
+        }
+
+        const data = values.map((value) => ({
+            ...value,
+            accountId: accountId as string,
+        }));
+
+        createTransactions.mutate(data, {
+            onSuccess: () => {
+                onCancelImport(); 
+            },
+        });
+    };
 
     if (transactionsQuery.isLoading) {
         return (
@@ -51,6 +94,19 @@ const TransactionsPage = () => {
         )
     }
 
+    if (variant === VARIANTS.IMPORT) {
+        return (
+          <>
+            <AccountDialog />
+            <ImportCard
+              data={importResults.data}
+              onCancel={onCancelImport}
+              onSubmit={onSubmitImport}
+            />
+          </>
+        );
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -58,10 +114,15 @@ const TransactionsPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Transaction History
                     </CardTitle>
-                    <Button onClick={newTransaction.onOpen} size = "sm">
-                        <Plus className="size-4 mr-2"></Plus>
-                        Add new
-                    </Button>
+                    <div className="flex items-center gap-x-2 flex-col lg:flex-row gap-y-2">
+                        <Button onClick={newTransaction.onOpen} size = "sm" className="w-full lg:w-auto">
+                            <Plus className="size-4 mr-2"></Plus>
+                            Add new
+                        </Button>
+                        <UploadButton
+                        onUpload={onUpload}
+                        />
+                    </div>
                 </CardHeader>
                 <CardContent>
                 <DataTable 
